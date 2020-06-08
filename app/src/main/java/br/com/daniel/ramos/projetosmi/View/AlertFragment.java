d2a88bb6c67908801437a6586edb014e533ef000
Daniel Ramos <danielmfr6@gmail.com>
2020-06-08 08:52:11 -0300

    Initial commit

---
+++ app/src/main/java/br/com/daniel/ramos/projetosmi/View/AlertFragment.java
@@ -0,0 +1,22 @@
+package br.com.daniel.ramos.projetosmi.View;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import br.com.daniel.ramos.projetosmi.R;
+
+public class AlertFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_alert, container, false);
+    }
+
+}
---
+++ app/src/main/java/br/com/daniel/ramos/projetosmi/View/MainActivity.java
@@ -0,0 +1,102 @@
+package br.com.daniel.ramos.projetosmi.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+import br.com.daniel.ramos.projetosmi.R;
+import br.com.daniel.ramos.projetosmi.View.AboutusFragment;
+import br.com.daniel.ramos.projetosmi.View.AlertFragment;
+import br.com.daniel.ramos.projetosmi.View.CallFragment;
+import br.com.daniel.ramos.projetosmi.View.DashboardFragment;
+import br.com.daniel.ramos.projetosmi.View.LocalizationFragment;
+import br.com.daniel.ramos.projetosmi.View.ReportsFragment;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toogle);
+        toogle.syncState();
+        /* Por padrão abrimos o app no Dashboard */
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new DashboardFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_dashboard);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_dashboard:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new DashboardFragment()).commit();
+                break;
+
+            case R.id.nav_reports:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ReportsFragment()).commit();
+                break;
+
+            case R.id.nav_localization:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new LocalizationFragment()).commit();
+                break;
+
+            case R.id.nav_call:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new CallFragment()).commit();
+                break;
+
+            case R.id.nav_alert:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new AlertFragment()).commit();
+                break;
+
+            case R.id.nav_bluetooth:
+                    Toast.makeText(this, "O bluetooth nav foi clicado", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_aboutus:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new AboutusFragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
